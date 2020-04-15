df388e491ffaeaef90c6db72e08d107f04117c4d
Rose Gaban <rosegaban@gmail.com>
2020-04-15 01:28:27 -0700

    Updates

---
+++ precache-manifest.8993855cb8e9769d84b4f911ef7b18b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e7096cd3e762e90fe6ce01d96fbe5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad72e9f10fff47689a47",
+    "url": "/monsters-rolodex/static/css/main.d7cf9919.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad72e9f10fff47689a47",
+    "url": "/monsters-rolodex/static/js/main.3c905236.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
